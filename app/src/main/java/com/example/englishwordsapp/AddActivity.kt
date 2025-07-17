a570b8337330a64fd7117119a91824035e007ffa
firekat <sp2007pavlov@yandex.ru>
2025-07-17 22:15:20 +0300

    adding a window for adding a target and some minor design improvements

--- app/src/main/java/com/example/englishwordsapp/AddActivity.kt
+++ app/src/main/java/com/example/englishwordsapp/AddActivity.kt
@@ -1,26 +1,16 @@
 package com.example.englishwordsapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val linkToAdd: TextView = findViewById(R.id.buttonAdd)
-
-        linkToAdd.setOnClickListener {
-            val intent = Intent(this, AddActivity::class.java)
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.activity_add)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/englishwordsapp/Plans.kt
@@ -0,0 +1,4 @@
+package com.example.englishwordsapp
+
+class Plans(var id: Int, var title: String, var date: String, var published: String) {
+}
